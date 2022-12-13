a397b35ae357b744c0793507d87f48a1b0c97022
Nikolay Arbuzov <anr.qwert@gmail.com>
2022-12-13 12:16:35 +0300

    Debug

--- TranspMatrix/main.c
+++ TranspMatrix/main.c
@@ -28,32 +28,212 @@ int main(int argc, char **argv) {
 
     MPI_Comm_rank(cart_comm, &my_rank);
     MPI_Comm_size(cart_comm, &n_proc);
-    //printf("My id = %2d, n_proc = %2d\n", my_rank, n_proc);
+    
     
     srand(my_rank + size);
-    int send_buf[size * size], recv_buf[size * size];
+    int send_buf[size * size][size * size], recv_buf[size * size][size * size], result[size * size];
     for (int i = 0; i < size * size; i++) {
-        send_buf[i] = rand();
-        recv_buf[i] = send_buf[i];
+        for (int j = 0; j < size * size; j++) 
+            if (i == my_rank){
+                send_buf[i][j] = rand() % 10;
+                recv_buf[i][j] = send_buf[i][j];
+            }
+            else { 
+                send_buf[i][j] = 0;
+                recv_buf[i][j] = send_buf[i][j];
+            }
+        result[i] = send_buf[my_rank][i];
     }
-    
-    
     MPI_Request send_r[size * size];
     
-    if (my_rank == 3){
-        printf("%d:\n", my_rank);
-        for (int i = 0; i < size*size; i++){
-            printf("%5d: %5d\n", i, send_buf[i]);
-        }
-        printf("\n");
-    }
     MPI_Barrier(MPI_COMM_WORLD);
     
-    //printf("My id = %2d, getting my coords\n", my_rank);
-
+    
+    int source, dest;
+    MPI_Status status;
     int my_coords[2];
     MPI_Cart_coords(cart_comm, my_rank, ndims, my_coords);
+    int counter = 1;
+    
+    // Step 1
+    if (my_coords[0] == 0 || my_coords[0] == 3){
+        if (my_coords[0] == 0)
+            MPI_Cart_shift(cart_comm, 0, 1, &source, &dest);
+        else 
+            MPI_Cart_shift(cart_comm, 0, -1, &source, &dest);
+        MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        
+    } else {
+        if (my_coords[0] == 1)
+            MPI_Cart_shift(cart_comm, 0, 1, &source, &dest);
+        else 
+            MPI_Cart_shift(cart_comm, 0, -1, &source, &dest);
+        MPI_Recv(recv_buf, size*size*size*size, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+        for (int i = 0; i < size*size; i++){
+                for (int j = 0; j < size*size; j++){
+                    if (recv_buf[i][j] != 0 && send_buf[i][j] == 0) {
+                        send_buf[i][j] = recv_buf[i][j];
+                    }
+                }
+            }
+        
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    // Step 2
+    
+    if (my_coords[0] == 1 || my_coords[0] == 2){
+        if (my_coords[1] == 0 || my_coords[1] == 3){
+            if (my_coords[1] == 0)
+                MPI_Cart_shift(cart_comm, 1, 1, &source, &dest);
+            else 
+                MPI_Cart_shift(cart_comm, 1, -1, &source, &dest);
+            
+            MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        } else {
+            if (my_coords[1] == 1)
+                MPI_Cart_shift(cart_comm, 1, 1, &source, &dest);
+            else 
+                MPI_Cart_shift(cart_comm, 1, -1, &source, &dest);
+            
+            MPI_Recv(recv_buf, size*size*size*size, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+            
+            for (int i = 0; i < size*size; i++){
+                for (int j = 0; j < size*size; j++){
+                    if (recv_buf[i][j] != 0 && send_buf[i][j] == 0) {
+                        send_buf[i][j] = recv_buf[i][j];
+                    }
+                }
+            }
+        }
+        
+    } 
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Step 3-4
+    for (int i = 0; i < 2; i++){
+        if ((my_coords[0] == 1 || my_coords[0] == 2) 
+            && (my_coords[1] == 1 || my_coords[1] == 2)) {
+            if (my_coords[0] == 1)
+                MPI_Cart_shift(cart_comm, 0, 1, &source, &dest);
+            else 
+                MPI_Cart_shift(cart_comm, 0, -1, &source, &dest);
+            MPI_Sendrecv(send_buf, size*size*size*size, MPI_INT, dest, 0,
+                        recv_buf, size*size*size*size, MPI_INT, dest, 0,
+                        MPI_COMM_WORLD, &status);
+            for (int i = 0; i < size*size; i++){
+                for (int j = 0; j < size*size; j++){
+                    if (recv_buf[i][j] != 0 && send_buf[i][j] == 0) {
+                        send_buf[i][j] = recv_buf[i][j];
+                    }
+                }
+            }
+            if (my_coords[1] == 1)
+                MPI_Cart_shift(cart_comm, 1, 1, &source, &dest);
+            else 
+                MPI_Cart_shift(cart_comm, 1, -1, &source, &dest);
+            MPI_Sendrecv(send_buf, size*size*size*size, MPI_INT, dest, 0,
+                        recv_buf, size*size*size*size, MPI_INT, dest, 0,
+                        MPI_COMM_WORLD, &status);
+            for (int i = 0; i < size*size; i++){
+                for (int j = 0; j < size*size; j++){
+                    if (recv_buf[i][j] != 0 && send_buf[i][j] == 0) {
+                        send_buf[i][j] = recv_buf[i][j];
+                    }
+                }
+            }
+        } 
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    // Step 5
+    
+    if ((my_coords[0] !=0 && my_coords[0] != 3)
+        || (my_coords[1] != 0 && my_coords[1] != 3)){
+        
+                    
+        if (my_coords[0] == 0 || my_coords[0] == 3 
+            || my_coords[1] == 0 || my_coords[1] == 3){
+
+            if (my_coords[0] == 0)
+                MPI_Cart_shift(cart_comm, 0, -1, &source, &dest);
+            else if (my_coords[0] == 3)
+                MPI_Cart_shift(cart_comm, 0, 1, &source, &dest);
+            else if (my_coords[1] == 0)
+                MPI_Cart_shift(cart_comm, 1, -1, &source, &dest);
+            else
+                MPI_Cart_shift(cart_comm, 1, 1, &source, &dest); 
+            MPI_Recv(recv_buf, size*size*size*size, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+            for (int i = 0; i < size*size; i++){
+                for (int j = 0; j < size*size; j++){
+                    if (recv_buf[i][j] != 0 && send_buf[i][j] == 0) {
+                        send_buf[i][j] = recv_buf[i][j];
+                    }
+                }
+            }
+            
+            
+        } else
+            if (my_coords[0] == 1)
+                if (my_coords[1] == 1){
+                    MPI_Cart_shift(cart_comm, 0, -1, &source, &dest);
+                    MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                    MPI_Cart_shift(cart_comm, 1, -1, &source, &dest);
+                    MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                } else {
+                    MPI_Cart_shift(cart_comm, 0, -1, &source, &dest);
+                    MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                    MPI_Cart_shift(cart_comm, 1, 1, &source, &dest);
+                    MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                } 
+            else 
+                if (my_coords[1] == 1){
+                    MPI_Cart_shift(cart_comm, 0, 1, &source, &dest);
+                    MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                    MPI_Cart_shift(cart_comm, 1, -1, &source, &dest);
+                    MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                } else {
+                    MPI_Cart_shift(cart_comm, 0, 1, &source, &dest);
+                    MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                    MPI_Cart_shift(cart_comm, 1, 1, &source, &dest);
+                    MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                }
+    } 
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Step 6
+    if (my_coords[0] == 0 || my_coords[0] == 3){
+        if (my_coords[1] == 0 || my_coords[1] == 3){
+            if (my_coords[1] == 0)
+                MPI_Cart_shift(cart_comm, 1, -1, &source, &dest);
+            else 
+                MPI_Cart_shift(cart_comm, 1, 1, &source, &dest);
+            MPI_Recv(recv_buf, size*size*size*size, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+            for (int i = 0; i < size*size; i++){
+                for (int j = 0; j < size*size; j++){
+                    if (recv_buf[i][j] != 0 && send_buf[i][j] == 0) {
+                        send_buf[i][j] = recv_buf[i][j];
+                    }
+                }
+            }
+            
+        } else {
+            if (my_coords[1] == 1)
+                MPI_Cart_shift(cart_comm, 1, -1, &source, &dest);
+            else 
+                MPI_Cart_shift(cart_comm, 1, 1, &source, &dest);
+            MPI_Send(send_buf, size*size*size*size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        }
+    } 
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
     //printf("My id = %2d, my coords is <%2d, %2d>\n", my_rank, my_coords[0], my_coords[1]);
+    /* // Способ с перессылкой для от одного всем
     for (int i = 0; i < size; i++){
         for (int j = 0; j < size; j++){
             if ((i != 0) || (j != 0)){
@@ -66,7 +246,6 @@ int main(int argc, char **argv) {
             }
         }
     }
-
 
     MPI_Barrier(MPI_COMM_WORLD);
     for (int i = 0; i < size; i++){
@@ -85,8 +264,24 @@ int main(int argc, char **argv) {
         if (i != my_rank)
             MPI_Wait(&send_r[i], MPI_STATUS_IGNORE);
     }
+    */
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (my_rank == 0) 
+    for (int i = 0; i < size * size; i++) {
+        for (int j = 0; j < size * size; j++) 
+            printf("%3d  ", send_buf[i][j]);
+        printf("\n");
+    }
     
-    printf("%5d: %5d\n", my_rank, recv_buf[3]);
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (my_rank == 0) 
+        printf("\n");
+    if (my_rank == 3) 
+    for (int i = 0; i < size * size; i++) {
+        for (int j = 0; j < size * size; j++) 
+            printf("%3d  ", send_buf[i][j]);
+        printf("\n");
+    }
 
     MPI_Finalize();
     return 0;
